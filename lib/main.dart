6c3daa12fff4228f4a512fc55f9f7eea907b043a
Berkan Enes Bican <berkanbican@hotmail.com>
2021-12-02 12:26:10 +0300

    Money Manager App

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// my own packages
+import 'package:moneymanagerapp/pages/home.dart';
+
+main() {
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
+}
